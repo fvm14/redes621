1314e2b721ccd3a099b62a60894a9b7529365820
Fabrizio Vargas <fabrialvargas@gmail.com>
2024-06-23 00:59:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def calcular_ruta():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
