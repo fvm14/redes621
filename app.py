dd689118a7b52fdd06620eae52c2b60e784bc612
Fabrizio Vargas <fabrialvargas@gmail.com>
2024-07-08 10:33:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,19 +28,18 @@ def calcular_rutaDijkstra():
         nodo_inicial = request.form['nodo_inicial']
         nodo_final = request.form['nodo_final']
 
-        # Verificar que los nodos existen en el grafo
+        
         if nodo_inicial not in grafo.vertices or nodo_final not in grafo.vertices:
             error_message = "Uno o ambos nodos no existen en el grafo. Por favor, ingrese nodos válidos."
             return render_template('index.html', error_message=error_message, image_data=grafo.graficar())
 
-        # Aplicar algoritmo de Dijkstra
         grafo.dijkstra(nodo_inicial)
         camino, distancia = grafo.camino(nodo_inicial, nodo_final)
 
-        # Generar imagen con el camino encontrado
+        
         image_data = grafo.graficarCamino(camino)
 
-        # Reiniciar estado de los vértices para futuros cálculos
+        
         grafo = Grafica()
 
         return render_template('ruta.html', camino=camino, distancia=distancia, image_data=image_data)
@@ -63,15 +62,15 @@ def calcular_rutaBellmanFord():
         nodo_inicial = request.form['nodo_inicial']
         nodo_final = request.form['nodo_final']
 
-        # Verificar que los nodos existen en el grafo
+       
         if nodo_inicial not in bellman_ford_grafo.G.nodes or nodo_final not in bellman_ford_grafo.G.nodes:
             error_message = "Uno o ambos nodos no existen en el grafo. Por favor, ingrese nodos válidos."
             return render_template('bellmanford.html', error_message=error_message, image_data=bellman_ford_grafo.graficar())
 
-        # Aplicar algoritmo de Bellman-Ford
+        
         camino, distancia = bellman_ford_grafo.bellman_ford(nodo_inicial, nodo_final)
 
-        # Generar imagen con el camino encontrado
+       
         image_data = bellman_ford_grafo.graficarCamino(camino)
 
         return render_template('rutabf.html', camino=camino, distancia=distancia, image_data=image_data)
